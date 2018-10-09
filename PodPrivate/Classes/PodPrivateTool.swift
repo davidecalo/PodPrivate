8fad89844d1d6234d5dd5e18bcaf784d7362c984
Calò Davide <Davide.Calo@nttdata.com>
2018-10-09 17:49:44 +0200

    [] - add class

---
+++ PodPrivate/Classes/PodPrivateTool.swift
@@ -0,0 +1,23 @@
+//
+//  PodPrivateTool.swift
+//  PodPrivate
+//
+//  Created by Calò Davide on 09/10/2018.
+//
+
+import Foundation
+public class PodPrivateTool{
+	
+	public init(){
+		
+	}
+	
+	public func Test(){
+		print("loremipsumdolorsitamet")
+	}
+	
+	public func Test2(){
+		print("loremipsumdolorsitamet2")
+
+	}
+}
