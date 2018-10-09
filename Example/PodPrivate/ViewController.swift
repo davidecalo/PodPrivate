88d0824c195310c51c1aac797260713eaf5f8c92
Calò Davide <Davide.Calo@nttdata.com>
2018-10-09 16:40:45 +0200

    [] - first commit /  init

---
+++ Example/PodPrivate/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PodPrivate
+//
+//  Created by davide.calo@nttdata.com on 10/09/2018.
+//  Copyright (c) 2018 davide.calo@nttdata.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
